d30a91e2a0aa3abd62f47140f924d922ed4513a6
Dustin Walker <thewalkman1234@gmail.com>
2017-11-05 16:52:54 -0600

    passport-init rabbithole

--- app.js
+++ app.js
@@ -2,27 +2,25 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var passport = require('passport');
-var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
 
-//Intilaizing Passport
-var initPassport = require('./passort-initialize');
-initPassport(passport);
-
+//mongoose stuff
+require('./models/models.js');
 var api = require('./routes/api');
 var authenticate = require('./routes/authenticate')(passport);
 var mongoose = require('mongoose');
 //connect to mongodb
 mongoose.connect("mongodb://localhost:27017/derp");
-require('./models/models.js');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -37,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use('/', index);
 app.use('/api', api);
 app.use('/auth', authenticate);
 
@@ -47,15 +46,31 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//Intilaizing Passport
+var initPassport = require('./passport-initialize');
+initPassport(passport);
 
-  // render the error page
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+          message: err.message,
+          error: err
+      });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+      message: err.message,
+      error: {}
+  });
 });
+
 
 module.exports = app;
