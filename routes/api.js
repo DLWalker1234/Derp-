b0ea11e6376bcf6acb0de7ba065a0a150db729f0
Dustin Walker <thewalkman1234@gmail.com>
2017-11-04 19:46:49 -0500

    routes

--- routes/api.js
+++ routes/api.js
@@ -14,4 +14,22 @@ router.route('/posts')
       res.send({message: 'crease a post dude.'})
     });
 
+router.route('/posts/:id')
+
+    //return one post
+    .get(function(req, res){
+      
+      res.send({message: 'return a post' + req.params.id})
+    })
+
+    .put(function(req, res){
+
+      res.send({message: 'need to do' + req.params.id})
+    });
+
+    .delete(function(req, res){
+
+      res.send({message: 'NEED TO DELETE' + req.params.id})
+    })
+
 module.exports = router;
